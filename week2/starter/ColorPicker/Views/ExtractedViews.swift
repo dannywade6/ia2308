128956ccd041228144ed5627d22c82d1f92ae8ee
DannyWade6 <Danny.Lee.Wade@gmail.com>
2023-09-10 20:41:41 +0100

    Move extracted views to new file

--- week2/starter/ColorPicker/Views/ExtractedViews.swift
+++ week2/starter/ColorPicker/Views/ExtractedViews.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,40 +32,63 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State private var alertIsVisible: Bool = false
-  @State private var redColor: Double = 0.0
-  @State private var greenColor: Double = 0.0
-  @State private var blueColor: Double = 0.0
-  @State private var foregroundColor = Color(red: 0, green: 0, blue: 0)
-  
-  // My work
-  @State private var colorSelection = ColorSelection()
+struct MainColorView: View {
+  var title: String
+  @Binding var foregroundColor: Color
   
   var body: some View {
-    
     VStack {
-      MainColorView(title: "Color Picker", foregroundColor: $foregroundColor)
-      SliderView(sliderValue: $redColor, colorName: "Red")
-        .tint(.red)
-      SliderView(sliderValue: $greenColor, colorName: "Green")
-        .tint(.green)
-      SliderView(sliderValue: $blueColor, colorName: "Blue")
-        .tint(.blue)
-      SetColorButtonView(redColor: $redColor, greenColor: $greenColor, blueColor: $blueColor, foregroundColor: $foregroundColor)
+      Text("Color Picker")
+        .font(.largeTitle)
+        .fontWeight(.bold)
+      
+      RoundedRectangle(cornerRadius: 0)
+        .foregroundColor(foregroundColor)
+        .border(Color.brown, width: 5)
     }
-    .padding(20)
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    Group {
-      ContentView()
-        .previewDisplayName("Light Mode")
-      ContentView()
-        .preferredColorScheme(.dark)
-        .previewDisplayName("Dark Mode")
+struct SliderView: View {
+  @Binding var sliderValue: Double
+  var colorName: String
+  
+  var body: some View {
+    VStack {
+      Text(colorName)
+        .padding(.bottom, 2)
+      HStack {
+        Slider(value: $sliderValue, in: 0...255)
+        Text("\(Int(sliderValue.rounded()))")
+      }
     }
+  }
+}
+
+struct SetColorButtonView: View {
+  @Binding var redColor: Double
+  @Binding var greenColor: Double
+  @Binding var blueColor: Double
+  @Binding var foregroundColor: Color
+  
+  var body: some View {
+    Button {
+      foregroundColor = Color(red: redColor / 255, green: greenColor / 255, blue: blueColor / 255)
+    } label: {
+      Text("Set Color")
+        .padding()
+        .foregroundColor(.white)
+    }
+    .background(
+      ZStack {
+        Color.blue
+        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+      }
+    )
+    .overlay(
+      RoundedRectangle(cornerRadius: 21)
+        .strokeBorder(Color.white, lineWidth: 2)
+    )
+    .cornerRadius(21)
   }
 }
